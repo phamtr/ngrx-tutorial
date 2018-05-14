1cda1270cd6e25e1da3f886d536890dcd2a07a24
Pham Van Truong <pvtruong115@gmail.com>
2018-05-14 16:31:24 +0700

    First commit

---
+++ src/app/read/read.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { Tutorial } from './../models/tutorial.model';
+import { AppState } from './../app.state';
+// Import our actions at the top
+import * as TutorialActions from './../actions/tutorial.actions';
+
+
+@Component({
+  selector: 'app-read',
+  templateUrl: './read.component.html',
+  styleUrls: ['./read.component.css']
+})
+export class ReadComponent implements OnInit {
+
+    // Section 1
+    tutorials: Observable<Tutorial[]>;
+
+    // Section 2
+    constructor(private store: Store<AppState>) { 
+      this.tutorials = store.select('tutorial');
+    }
+    delTutorial(index) {
+      this.store.dispatch(new TutorialActions.RemoveTutorial(index) )
+    }
+  
+    ngOnInit() {}
+
+}
